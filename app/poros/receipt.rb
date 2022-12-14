406748a4432e9da515dfae47ac78712142c5bf84
Ted Staros <tstaros23@gmail.com>
2022-12-13 20:26:22 -0700

    Finish creating receipt object and tests pass

--- app/poros/receipt.rb
+++ app/poros/receipt.rb
@@ -1,4 +1,14 @@
+require 'securerandom'
+
 class Receipt
+  attr_reader :retailer, :purchase_date, :purchase_time, :total, :short_description, :price, :id
   def initialize(attr)
+    @retailer = attr[:retailer]
+    @purchase_date = attr[:purchase_date]
+    @purchase_time = attr[:purchase_time]
+    @total = attr[:total]
+    @short_description = attr[:items][0][:short_description]
+    @price = attr[:items][0][:price]
+    @id = SecureRandom.hex
   end
 end
--- spec/poros/receipt_spec.rb
+++ spec/poros/receipt_spec.rb
@@ -7,31 +7,30 @@ RSpec.describe Receipt do
       retailer: "Target",
       purchase_date: "2022-01-02",
       purchase_time: "13.13",
-      total: "1.25",
+      total: 1.25,
       items: [
-        {short_description: "Pepsi - 12-oz", price: "1.25"}
+        {short_description: "Pepsi - 12-oz", price: 1.25}
       ]
     }
     processed_receipt = Receipt.new(attr)
 
     expect(processed_receipt).to be_an_instance_of(Receipt)
 
-     expect(receipt.retailer).to eq("Target")
+     expect(processed_receipt.retailer).to eq("Target")
      expect(processed_receipt.retailer.class).to eq(String)
 
      expect(processed_receipt.purchase_date).to eq("2022-01-02")
      expect(processed_receipt.purchase_date.class).to eq(String)
 
      expect(processed_receipt.total).to eq(1.25)
-     expect(processed_receipt.total.class).to eq(Integer)
+     expect(processed_receipt.total.class).to eq(Float)
 
-     expect(hourly_weather.short_description).to eq("Pepsi - 12-oz")
-     expect(hourly_weather.short_description.class).to eq(String)
+     expect(processed_receipt.short_description).to eq("Pepsi - 12-oz")
+     expect(processed_receipt.short_description.class).to eq(String)
 
-     expect(hourly_weather.short_description).to eq(1.25)
-     expect(hourly_weather.short_description.class).to eq(Integer)
+     expect(processed_receipt.price).to eq(1.25)
+     expect(processed_receipt.price.class).to eq(Float)
 
-     expect(hourly_weather.id).to eq(nil)
-     expect(hourly_weather.id.class).to eq(NilClass)
+     expect(processed_receipt.id.class).to eq(String)
   end
 end
