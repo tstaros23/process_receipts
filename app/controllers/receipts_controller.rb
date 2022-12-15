3c3624b6d59c2c64f73b1b86139da9ca84db2a82
Ted Staros <tstaros23@gmail.com>
2022-12-14 17:26:11 -0700

    Create low level caching and work on fetching the receipt from the create action

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,13 +1,19 @@
 class ReceiptsController < ApplicationController
   def show
-    receipt = Receipt
+    # receipt = Rails.cache.fetch("receipts/#{params[:id]}/points", expires_in: 12.hours)
+    receipt = Rails.cache.read("#{receipt_params[:id]}")
+    receipt2 = Rails.cache.fetch(receipt_params, expires_in: 12.hours) do
+      receipt_params[:id] == params[:id]
+    end
   end
+
   def create
     receipt = Receipt.new(receipt_params)
+    Rails.cache.write("#{receipt.id}", receipt)
+    Rails.cache.read("#{receipt.id}")
     render json: ReceiptSerializer.format_processor(receipt), status: :created
   end
 
-  private
 
   def receipt_params
     params.permit(:retailer, :purchase_date, :purchase_time, :total, :short_description, :price, :id, items: [:short_description, :price])
